33a9b6131889f2244457f57ed80e8d787b29dc23
ndshapi1 <90224265+ndshapi1@users.noreply.github.com>
2023-09-06 22:49:20 -0700

    Update Bins.m

--- Utilities/Bins.m
+++ Utilities/Bins.m
@@ -7,21 +7,23 @@ function [x2,Y2_cell, num_max,Total_side] = Bins(x1,y1, incr, xlen, rad,SystemPa
 
 xa = x1(:,1);
 indexes=find(y1);
-xa_short=xa(indexes,1).*10^-4;
+xa_short=xa(indexes,1).*10^-4;%x position in cm
 y1_short=y1(indexes);
-radius=rad*1e-4;%radius
-Total_side=sum(y1,'all');
-incr=incr*10^-4;
+incr=incr*10^-4;%increment in cm
+radius=rad*1e-4;%radius in cm
 
+%if there's an SMA connector, the x vector will record differently (x
+%within the sma connect is negative, the 0 starts outside the SMA
+%connector)
 if SystemParam.SMA==1
     num_max = floor(((xlen/10e3)-(SystemParam.SMA_totallength/10e3))/(incr))+1;%dividing length of fiber by the increments, then adding one to have data at each end of bin
     x2 = zeros(1,num_max+2);
     y2 = zeros(1,num_max+2);
-
-    %first two increments are within the SMA flush length
+    
+    %first two measurement points will be within the SMA flush length
     Inc1=SystemParam.SMA_flushlength*10^-4;
     Inc2=(SystemParam.SMA_totallength-SystemParam.SMA_flushlength)*10^-4;
-
+    
     %first two points have irregular indexes if a part of the sma connector
     x2(1)=-(Inc2+Inc1);
     x2(2)=-Inc1;
@@ -31,17 +33,23 @@ if SystemParam.SMA==1
     indX2=intersect(indX2_1,indX2_2);%logical intersection
     totalY1=sum(y1_short(indX1));
     totalY2=sum(y1_short(indX2));
-    y2(1)=totalY1/(2*pi*Inc1*radius);
-    y2(2)=totalY2/(2*pi*Inc2*radius);
-    start_pos=3;
-    adj_for_sma=1;
+    y2(1)=totalY1;%/(2*pi*Inc1*radius);
+    y2(2)=totalY2;%/(2*pi*Inc2*radius);
+    start_pos=3;%the initial vector
+    adj_for_sma=1;%don't adjust the start and stop indexes for the sma connector
+    
 else%if no sma connector
     num_max = floor((xlen/10e3)/(incr))+1;%dividing length of fiber by the increments, then adding one to have data at each end of bin
     x2 = zeros(1,num_max);
     y2 = zeros(1,num_max);
     start_pos=1;
     adj_for_sma=0;%don't adjust the start and stop indexes for the sma connector
-end   
+end
+
+
+Total_side=sum(y1,'all');%.*2*pi*radius*xlen/10e3;%power in W
+
+
 
 for i=0:num_max-1
     ind2=i+start_pos;%start at the 3rd index in the x2 and y2 vectors
@@ -53,9 +61,22 @@ for i=0:num_max-1
     indxa=intersect(indxa_1,indxa_2);%logical intersection
     
     totaly2=sum(y1_short(indxa));
-    y2(ind2)=totaly2/(2*pi*incr*radius);
+    y2(ind2)=totaly2;%/(2*pi*incr*radius);
 end
 
+% for i=0:num_max-1
+%     start = incr*i;
+%     stop = start+incr;
+%     total = 0;
+%     for a=1:length(xa_short)
+%         if (xa_short(a)>=start) && (xa_short(a) <= stop) && (y1_short(a) > 0)
+%             total = total + y1_short(a);
+%         end
+%     end
+%     x2(i+1) = start;
+%     y2(i+1) = total/(2*pi*incr*radius);% sum of light divided by surface area of section
+% end
+%setting final value
 if adj_for_sma==1
     x2(end)=incr*i;
 else
