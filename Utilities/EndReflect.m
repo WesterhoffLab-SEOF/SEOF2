29d2fb1ac4390d3121b468c918c5067da18de1c0
ndshapi1 <90224265+ndshapi1@users.noreply.github.com>
2023-09-06 22:51:17 -0700

    Update EndReflect.m

--- Utilities/EndReflect.m
+++ Utilities/EndReflect.m
@@ -20,36 +20,38 @@ end
 iteration=Global_Index(1);h=Global_Index(2);aa=Global_Index(3);%indexes
 %make sure the ray isn't vertical
 xtoy=V_in(1)/V_in(2);
-if isreal(xtoy)&& xtoy==0%if the ray is entirely vertical, it only side emits
-    Tally.photon_vert_count(iteration,h,aa)=Tally.photon_vert_count(iteration,h,aa)+1;%counter goes up
-    %record the values
-    meas.points(meas.counter,:)=P_in;
-    meas.inten(meas.counter,:)=I_in;
-    meas.counter=meas.counter+1;
-    meas.sum=I_in+meas.sum;
-    %set up output variables
-    I_trans=0;
-    I_scat=0;
-    Theta_scat=0;
-    direction=directioni;
-    b2h=0;
-    [Tally,~,~] = DifTrack(I_in,[I_scat,I_trans,(sum(meas.inten)-Meas0)],SystemParam,'Vertical cond w/in EndReflect',1,Global_Index,Tally);
-        [Tally,~,~] = inOutTrack(I_in,(sum(meas.inten)-Meas0),SystemParam,'Vertical cond w/in EndReflect',1,Global_Index,Tally);
-
-    return%exit the function, go back to the main function
-end
-[theta_i,theta_t,theta_c,theta_r,~,~,direction] = Snells(V_in,nhat,ni,nt,horz_surf,directioni);
+% if isreal(xtoy)&& xtoy==0%if the ray is entirely vertical, it only side emits
+%     Tally.photon_vert_count(iteration,h,aa)=Tally.photon_vert_count(iteration,h,aa)+1;%counter goes up
+%     %record the values
+%     meas.points(meas.counter,:)=P_in;
+%     meas.inten(meas.counter,:)=I_in;
+%     meas.counter=meas.counter+1;
+%     meas.sum=I_in+meas.sum;
+%     %set up output variables
+%     I_trans=0;
+%     I_scat=0;
+%     Theta_scat=0;
+%     direction=directioni;
+%     b2h=0;
+%     [Tally,~,~] = DifTrack(I_in,[I_scat,I_trans,(sum(meas.inten)-Meas0)],SystemParam,'Vertical cond w/in EndReflect',1,Global_Index,Tally);
+%         [Tally,~,~] = inOutTrack(I_in,(sum(meas.inten)-Meas0),SystemParam,'Vertical cond w/in EndReflect',1,Global_Index,Tally);
+% 
+%     return%exit the function, go back to the main function
+% end
+% disp(V_in)
+% disp(atan(V_in(2)/V_in(1)))
+[theta_i,theta_t,theta_c,theta_ih,theta_rh,~,~,direction] = Snells(V_in,nhat,ni,nt,horz_surf,directioni);
 %direction here should flip from the original value due to the x directions
 if directioni*direction~=-1
     error("direction didn't flip inside the end reflect value")
 end
 %of the incoming and leaving velocities being opposite
-[I_reflect,Itrans]=FresnelEq(I_in,SystemParam,theta_i,theta_t,theta_c,ni,nt);
+[I_reflect,Itrans]=FresnelEq(I_in,SystemParam,theta_i,theta_t,theta_c,theta_ih,ni,nt,horz_surf);
 %check there isn't a large difference after using the fresnell eq. keep
 %track in Tally struct
 [Tally,~,~] =  DifTrack(I_in,[I_reflect,Itrans],SystemParam,'FresnelEq w/in EndReflect',1,Global_Index,Tally);
 [Tally,~,~] =  inOutTrack(I_in,Itrans,SystemParam,'FresnelEq w/in EndReflect',1,Global_Index,Tally);
-
+fres_dif=I_in-sum([I_reflect,Itrans]);
 if directioni==1%hitting the end of the fiber
     I_trans=Itrans;%transmitted light will go into the medium
     b2h=0;
@@ -58,15 +60,20 @@ else%hitting the coupling face of the fiber
     I_trans=0;
 end
 %find an appropriately sized scatter cone
-[I_scatter,Theta_scat]=scatter_cone(SystemParam,theta_r,vert_surf);
+% I_scatter=1;
+% Theta_scat=theta_rh;
+[I_scatter,Theta_scat]=scatter_cone(SystemParam,theta_rh,vert_surf);
 I_scat=I_scatter.*I_reflect;
+ref_dif=I_reflect-sum(I_scat);
 %check there isn't a large difference after scattering. keep
 %track in Tally struct
 [Tally,~,~] =  DifTrack(I_reflect,I_scat,SystemParam,'scatter_cone inside EndReflect',1,Global_Index,Tally);
 
 %update number of bounces
 bounce_num=bounce_num+1;
-
+pow_in=I_in;
+pow_out=[(sum(meas.inten)-Meas0),I_scat,I_trans,b2h];
+scat_dif=pow_in-sum(pow_out);
 [Tally,~,~] =  DifTrack(I_in,[(sum(meas.inten)-Meas0),I_scat,I_trans,b2h],SystemParam,'EndReflect total',1,Global_Index,Tally);
 
 end
