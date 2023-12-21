8e8e71e6abebe120a2a6acec5304b655dd00051c
ndshapi1 <90224265+ndshapi1@users.noreply.github.com>
2023-12-21 12:31:00 -0700

    Update ContLoss.m

--- Utilities/ContLoss.m
+++ Utilities/ContLoss.m
@@ -11,9 +11,10 @@ nt=real(nt1);
 ni_c=ni1;
 nt_c=nt1;
 dt=norm(dtrav);
-alpha_r=SystemParam.alpha_r;%db/um
-alpha_uv=SystemParam.alpha_uv;%db/um
-Absorb_in=(I_in*(1-10^(-alpha_uv*dt/10)));
+alpha=SystemParam.alpha;%db/um
+r_coeff=SystemParam.r_coeff;%coefficient of losses attributed to rayleigh
+losses=(I_in*(1-10^(-alpha*dt/10)));
+Absorb_in=losses*(1-r_coeff);
 
 %calculate remaining light after absorption. wait to remove more 
 I_rem1=I_in-Absorb_in;
@@ -27,25 +28,31 @@ deg=abs(rad2deg(theta_i));
 deg_index=round(SystemParam.ang_div*deg);%index for the angle
 %find the RI index 
 switch ni
-    case 1
-        ni_index=2;
-    case 1.333
-        ni_index=5;
-    case 1.353
-        ni_index=4;
-    case 1.5
+    case real(SystemParam.n1)
         ni_index=1;
+    case real(SystemParam.n2)
+        ni_index=2;
+    case real(SystemParam.n3)
+        ni_index=3;
+    case real(SystemParam.n4)
+        ni_index=4;
+    case real(SystemParam.nwater)
+        ni_index=5;
+    case real(SystemParam.n_metal)
+        ni_index=6;
 end
 
 switch nt
-    case 1
-        nt_index=2;
-    case 1.333
-        nt_index=5;
-    case 1.353
-        nt_index=4;
-    case 1.5
+    case real(SystemParam.n1)
         nt_index=1;
+    case real(SystemParam.n2)
+        nt_index=2;
+    case real(SystemParam.n3)
+        nt_index=3;
+    case real(SystemParam.n4)
+        nt_index=4;
+    case real(SystemParam.nwater)
+        nt_index=5;
     case real(SystemParam.n_metal)
         nt_index=6;
 end
@@ -55,7 +62,7 @@ scat_coeffs=cell2mat(SystemParam.scatter_coeff(ni_index,nt_index,deg_index));
 Forward=scat_coeffs(1);Backward=scat_coeffs(2);Sideup=scat_coeffs(3);Sidedown=scat_coeffs(4);Rayloss=scat_coeffs(5);
 %calculate amount of light scattered in each direction
 %assumed lost Rayleigh light
-RCloss_in=I_in*(1-10^(-alpha_r*dt/10));%amount 'lost (ie not going forward) along the travel distance
+RCloss_in=losses*r_coeff;%amount 'lost (ie not going forward) along the travel distance
 I_scat_tot=RCloss_in.*Rayloss;%take the ratio for hwo much total light is actually scattered
 I_forward=Forward*I_scat_tot;%add this back to the returning
 I_backscat=Backward*I_scat_tot;%this is the main light reported to be lost to general scatter
@@ -74,14 +81,14 @@ Y_down=-1*ydir*bound(2);
 Y_up=ydir*bound(2);
 %assume all scattered light is in the vertical direction
 theta_i=0;theta_t=0;theta_c=asin(nt/ni);theta_ih=pi/2;
-%calculate the amount of scattered light absorbed on the way to the surface
-%boundary
-I_scat_absorbedup=(I_scatup*(1-10^(-alpha_uv*dy_scatup/10)));
-I_scat_absorbeddown=(I_scatdown*(1-10^(-alpha_uv*dy_scatdown/10)));
+%calculate the amount of scattered light lost on the way to the surface
+%boundary. include losses for both absorb and rayleigh
+I_scat_absorbedup=(I_scatup*(1-10^(-alpha*dy_scatup/10)));
+I_scat_absorbeddown=(I_scatdown*(1-10^(-alpha*dy_scatdown/10)));
 IT.absorbi=I_scat_absorbedup+I_scat_absorbeddown+IT.absorbi;%record as absorbed light
-%remove the absorbed from up and down
-I_scat_inup=I_scatup*(10^(-alpha_uv*dy_scatup/10));
-I_scat_indown=(I_scatdown*(10^(-alpha_uv*dy_scatdown/10)));
+%remove the loss from up and down
+I_scat_inup=I_scatup*(10^(-alpha*dy_scatup/10));
+I_scat_indown=I_scatdown*(10^(-alpha*dy_scatdown/10));
 %do fresnell eq to see what transmits
 if any(I_scat_inup)% if the value isn't 0
     [I_scatup_reflect,I_scatup_transmit]=FresnelEq(I_scat_inup,SystemParam,theta_i,theta_t,theta_c,theta_ih,ni_c,nt_c,horz_surf);
@@ -125,11 +132,13 @@ V_sideemit=V_sideemit0(ind,:);
 I_losstot=I_scatterloss+Absorb_in;
 if isnan(I_remaining)
     error('nan in conttrav')
+elseif ~any(I_remaining)
+    
+    error('I remaining is empty')
 end
 % %  Pow_Out=[I_remaining,I_sideemit,(IT.cutoffi-Cutoff0),(IT.absorbi-Abs0),(IT.housi-House0),(IT.approxi-Approx0),(IT.backi-Back0),(IT.transi-Trans0),(IT.b2hi-B2h0)];
 % % [Tally,~,~] =  DifTrack(I_in,Pow_Out,SystemParam,'ContLoss total',1,Global_Index,Tally);
 % % [Tally,~,~] =  inOutTrack(I_in,I_sideemit,SystemParam,'ContLoss total',1,Global_Index,Tally);
 if I_remaining<0
-    disp(I_losstot)
-    disp(V_sideemit)
+    error('Negative I remaining in conttrav')
 end
